617a58d0f1bf7f880659cf41e043dc6c59ca5b4a
jaruserickson <jacob.erickson@mail.utoronto.ca>
2016-09-27 14:07:21 -0400

    added randompic script

---
+++ randompic.js
@@ -0,0 +1,8 @@
+window.onload = randPicture;
+
+var lib = new Array("img/hd/image-1.jpg","img/hd/image-2.jpg","img/hd/image-3.jpg","img/hd/image-4.jpg","img/hd/image-5.jpg","img/hd/image-6.jpg","img/hd/image-7.jpg","img/hd/image-8.jpg","img/hd/image-9.jpg","img/hd/image-10.jpg","img/hd/image-11.jpg","img/hd/image-12.jpg","img/hd/image-13.jpg");
+
+function randPicture() {
+     var randomNum = Math.floor(Math.random() * lib.length);
+     document.getElementById("return").src = lib[randomNum];
+ }
