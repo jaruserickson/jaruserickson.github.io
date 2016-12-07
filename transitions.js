b2d0d94d6089a70eb4daecc1ca5ee51cdb1536a0
Jacob <jacob.erickson@mail.utoronto.ca>
2016-12-06 22:50:17 -0500

    transitions fixed for safari finally - image still blurry post-keyframe

--- transitions.js
+++ transitions.js
@@ -11,12 +11,12 @@ $("#blurb").hide(); //so interaction will not be broken
 //this jquery is so messy
 $(document).on('click', function(e) { //on document click
     if ($(e.target).closest('#person').length) {
-    	
-
-    	$('#profile').stop().fadeTo(500, 1);
+    	console.log("click on");
+    	$('#me').stop().fadeTo(500, 1);
         $("#person").stop().fadeTo(300, 0);
-        $('#contact').removeClass("flipInY");
-        $('#contact').addClass("flipOutY");
+		$("#person").css("opacity", "0");
+        $('#contact').removeClass("flipInX");
+        $('#contact').addClass("flipOutX");
 
     	setTimeout(function (){
     		$("#blurb").show();
@@ -25,8 +25,12 @@ $(document).on('click', function(e) { //on document click
     	}, 300);
         flag = false;
     }else{
-		$('#contact').removeClass("flipOutY");
-    	$('#contact').addClass("flipInY");
+    	console.log("click off");
+		$('#contact').removeClass("flipOutX");
+    	$('#contact').addClass("flipInX");
+
+    	$("#person").removeClass("zoomIn");
+    	$("#person").addClass("zoomOut");
 
     	$("#blurb").removeClass("zoomIn");
         $('#blurb').addClass("zoomOut");
@@ -40,26 +44,34 @@ $(document).on('click', function(e) { //on document click
 //so so messy
 $("#contact").hover(function (){ //animation for hover on photo
 	if (flag){
-		$('#profile').stop().fadeTo(500, 0.2);
+		console.log("hover on");
+		$('#me').stop().fadeTo(500, 0.2);
 		$("#person").stop().fadeTo(300, 1);
+		$("#person").css("opacity", "1");
+
 		$("#person").removeClass("zoomOut");
 		$("#person").addClass("zoomIn");
 	}
 }, function (){
 	if (flag){
-		$('#profile').stop().fadeTo(500, 1);
+		console.log("hover off");
+		$('#me').stop().fadeTo(500, 1);
+		$("#person").stop().fadeTo(300, 0);
+		$("#person").css("opacity", "0");
+		
 		$("#person").removeClass("zoomIn");
 		$("#person").addClass("zoomOut");
 	}
 });
 
 /*
-$("#person").hover(function (){ //hover animation for icon
+$("#person").hover(function (){
 	if (flag){ $("#person").stop().fadeTo(300, 0.5); }
-}, function(){
+}, function (){
 	if (flag){ $("#person").stop().fadeTo(300, 1); }
 });
 */
+
 var aboutButton = document.getElementById("person");
 
 if (flag){
