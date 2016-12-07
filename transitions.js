36b2bfa5d9f8ed82a1b9e5b670f13256eaad3906
Jacob <jacob.erickson@mail.utoronto.ca>
2016-12-06 19:36:49 -0500

    some transitions transferred to css, some timings edited

--- transitions.js
+++ transitions.js
@@ -2,27 +2,37 @@
 	// - safari is SO BROKEN :(
 	// - get rid of cursor pointer when blurb is up ??
 		
-var flag = true;
+//solution
+	//i have css transitions and jquery running in tandem
+	//run them jquery -> css?
+var flag = true; //if flag is up then the about blurb is down
 $("#blurb").hide(); //so interaction will not be broken
 
 //this jquery is so messy
 $(document).on('click', function(e) { //on document click
     if ($(e.target).closest('#person').length) {
-    	$("#blurb").show();
-    	$("#blurb").removeClass("zoomOut");
-        $("#blurb").addClass("zoomIn");
+    	
 
-        $('#profile').stop().fadeTo(500, 1);
+    	$('#profile').stop().fadeTo(500, 1);
         $("#person").stop().fadeTo(300, 0);
         $('#contact').removeClass("flipInY");
         $('#contact').addClass("flipOutY");
+
+    	setTimeout(function (){
+    		$("#blurb").show();
+    		$("#blurb").removeClass("zoomOut");
+        	$("#blurb").addClass("zoomIn");
+    	}, 300);
         flag = false;
     }else{
-    	$("#blurb").hide();
 		$('#contact').removeClass("flipOutY");
     	$('#contact').addClass("flipInY");
+
     	$("#blurb").removeClass("zoomIn");
         $('#blurb').addClass("zoomOut");
+        setTimeout(function() {
+        $("#blurb").hide();
+    	}, 300);
         flag = true;
     }
 });
@@ -43,12 +53,13 @@ $("#contact").hover(function (){ //animation for hover on photo
 	}
 });
 
+/*
 $("#person").hover(function (){ //hover animation for icon
 	if (flag){ $("#person").stop().fadeTo(300, 0.5); }
 }, function(){
 	if (flag){ $("#person").stop().fadeTo(300, 1); }
 });
-
+*/
 var aboutButton = document.getElementById("person");
 
 if (flag){
